ec46b3d9fc95681569eb720c3c5b61e73446d767
amiruddin <mamiruddin34@gmail.com>
2021-09-02 11:29:34 +0800

    update public API and new path to compare

--- index.js
+++ index.js
@@ -9,12 +9,27 @@ getPublicIPv4 = async () => {
   return await publicIp.v4()
 }
 
+/*  returns client IP address when runs on local, but when deploys on server (heroku in this case),
+    always returns a same IP, because its requesting IP for itself
+*/
 app.get('/', async (req, res) => {
   try {
-    const ip = await getPublicIPv4();
+    const ip = await getPublicIPv4()
     res.status(200).json({ ip })
   } catch (err) {
     res.status(500).json({ message: err })
+  }
+})
+
+/*  returns a local IP if runs on localhost, but when deploys on server (heroku in this case),
+    will return correct IP address as per other services (ipify, geolocation-db, etc..).
+*/
+app.get('/ip', async (req, res) => {
+  try {
+    const ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress || '').split(',')[0].trim()
+    res.status(200).json({ ip })
+  } catch (err) {
+
   }
 })
 
