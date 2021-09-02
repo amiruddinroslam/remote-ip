7011910d446320a25c2659f2614d25a10935956a
amiruddin <mamiruddin34@gmail.com>
2021-09-02 11:16:25 +0800

    update public API

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const app = require('express')()
 const cors = require('cors')
+const publicIp = require('public-ip');
 const port = process.env.PORT || 8080
 
 app.use(cors())
 
+getPublicIPv4 = async () => {
+  return await publicIp.v4()
+}
+
 app.get('/', async (req, res) => {
   try {
-    const ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress || '').split(',')[0].trim();
+    const ip = await getPublicIPv4();
     res.status(200).json({ ip })
   } catch (err) {
     res.status(500).json({ message: err })
